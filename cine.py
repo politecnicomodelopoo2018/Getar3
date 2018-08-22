0759b3059f79632683bedd9232a588125bc07433
yitar <facundo.get2000@gmail.com>
2018-08-22 17:42:21 -0300

    Trabajo FInal de Laboratorio 5c

---
+++ cine.py
@@ -0,0 +1,8 @@
+from db import DB
+
+class Cine(object):
+    idCine = None
+    nombre = None
+
+    def Dar_de_Alta_Cine(self):
+        DB.run("Insert into Cine(idCine,nombre) VALUES(" + str(self.idCine) + ", '" + self.nombre + "');")
